d33baf912804d0761ac46a4c90b1174100c9d17b
Christian Dreier <flipper.x3r@googlemail.com>
2017-07-18 02:20:27 +0200

    Execute a string as PowerShell script

--- shortcut.py
+++ shortcut.py
@@ -2,8 +2,24 @@
 # -*- coding: utf-8 -*-
 
 
+import subprocess
+
+
+_test_script = """\
+for ($i = 1; $i -le 10; $i++) {
+    Write-Host "Iteration" + $i
+    Start-Sleep 1
+}
+"""
+
+
 def main():
-    pass
+    script = "{\n" + _test_script + "\n}"
+    process = subprocess.Popen(
+        # A "script block" can only be used if powershell is executed inside powershell
+        "powershell -Command powershell -Command " + script
+    )
+    process.wait()
 
 
 if __name__ == "__main__":
